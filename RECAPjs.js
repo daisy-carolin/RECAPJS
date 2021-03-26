8039e439198b8e96f36778b7633cd954bd258578
student <daisy-carolin@gmail.com>
2021-03-26 09:41:54 +0300

    first commit

---
+++ RECAPjs.js
@@ -0,0 +1,61 @@
+function pen(){
+    var number=20;
+    return number;
+    
+}
+pen();
+
+
+
+var number=10;
+function pen(){
+    return number;
+
+}
+console.log(number);
+
+function book(){
+    var number=50;
+    return number;
+}
+book();
+
+var number=50;
+function book(){
+    return number;
+}
+console.log(number);
+
+function getValues(){
+    return[1,2];
+}
+var values=getValues();
+
+var  first=values[0];
+var  second=values[1];
+
+console.log("first Value: "+first);
+console.log("second Value: "+second);
+
+function getValues(){
+    return [3,4];
+}
+var values=getValues();
+
+var third=values[0];
+var fourth=values[1];
+
+console.log("third Value: "+third);
+console.log("fourth Value: "+fourth);
+
+function getValues(){
+    return[5,6];
+}
+var Values=getValues();
+var fifth=values[0];
+var sixth=values[1];
+console.log("fifth Value: "+fifth);
+console.log("sixth Value: "+sixth);
+
+
+ 
